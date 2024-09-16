7d7e584ed0bc23e9ce1bb664315ae515fffb0c8f
Chan Pham <iamchanpham@gmail.com>
2024-09-16 01:33:55 +0000

    Lab template

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# nested-CRUD-lab
+# Nested CRUD Lab
+Complete the lab based on the instructions in the comments. Make sure to commit at the appropriate points and push to submit the assignment.
+
+## Rubric
+- Tasks (10 points)
+- Git (2 points)
+    - commit messages at the right point with appropriate messages
+    - submitting correctly by pushing
+- Extra Credit (2 points)
---
+++ index.js
@@ -0,0 +1,58 @@
+// ARRAY SECTION
+// Task 1: Create an empty array called playlist
+
+// Task 2: In a for loop that runs 3 times,
+// - Prompt the user to enter the name of a song
+// - Add the song to the playlist array
+
+
+// Task 3: Update the value of the last element to be "Test"
+
+
+// Task 4: Delete the "Test" element 
+
+// Console log the array
+
+
+// Commit your changes with an appropriate message.
+// ----------------------------------
+// OBJECTS SECTION
+// Task 5: Create an object called 'song' with 'title', 'artist', and 'duration' properties
+
+// Task 6: Add a property 'listenedTo' to the 'song' object and set it to false
+
+// Task 7: Prompt the user to ask if they listened to (title) by (artist). 
+// You should access these values through the object properties.
+
+// Write the condition such that they say yes, set listenedTo to be true
+
+
+// Commit your changes with an appropriate message.
+// ----------------------------------
+// NESTED ARRAYS & OBJECTS SECTION
+// Task 8: Create an object called 'album' with properties song's album's 'name', 'year', and 'songs' (empty array for now)
+
+// Task 9: Add your song object to the songs array in the album object
+
+// Console log the length of the album's songs array to confirm it worked
+
+// Task 10: Update the title of the song to be all lowercase. 
+// You need to go through the album object for credit.
+
+// Console log the album object to check your change
+
+// Commit your changes with an appropriate message.
+// ----------------------------------
+// EXTRA CREDIT
+// Prompt the user to enter the title, artist, and duration
+
+// Use the user info to create a new song object
+
+// Add it to the songs array in your album object
+
+// Print out the album information with the following formatting
+// "Album: [name] ([year])"
+
+// Loop through the songs array in the album object and print out the song information with the following formatting
+// "Song [index]: [title] ([duration])"
+
